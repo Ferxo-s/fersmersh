56bc04721ff88f719a6eb2b9707c364a7ae625ff
Fernando Grande Aguilella <fernandograndeaguilella@gamil.com>
2025-11-26 19:29:30 +0100

    Se añade el programa de popochos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    Scanner scan = new Scanner(System.in);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    System.out.println("Cuantos popochos tienes :");
+    int popochos = scan.nextInt();
+    scan.nextLine();
+    String ZZ="Zzz";
+    if (popochos==1){
+        System.out.println("El popocho esta demasiado mimado :3");
+    }
+    else if(popochos<=3){
+        System.out.println("La familia esta equilibrada <3");
+    }
+    else if (popochos>=4){
+        System.out.println("La familia tiene demasiados popochos NO HAY MIMOS PARA TODOS");
+    }
+    else {
+        System.out.println("No tienes ningun popocho ADOPTA UNO");
+    }
+    for (int i =1;i<=popochos;i++){
+        System.out.println("El popocho "+i+" esta dormido :"+ZZ);
+        ZZ+="Zzz";
     }
 }
